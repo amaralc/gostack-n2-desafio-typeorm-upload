18f4a2b604a4e0a24aa3d7d33c42ee557379daa6
amaralc <amaral.calil@gmail.com>
2020-12-20 15:06:06 -0300

    FEATURE / Adiciona metodos index e delete no controller

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateTransactionService from '../services/CreateTransactionService';
+import ListTransactionsService from '../services/ListTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 
 export default class TransactionController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -22,5 +24,30 @@ export default class TransactionController {
 
     /** Devolve resposta à requisição */
     return response.json(transaction);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    /** Instancia serviço utilizando container de injecao de dependencias */
+    const listTransactions = container.resolve(ListTransactionsService);
+
+    /** Executa serviço */
+    const transactions = await listTransactions.execute();
+
+    /** Devolve resposta à requisição */
+    return response.json(transactions);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    /** Instancia serviço utilizando container de injecao de dependencias */
+    const deleteTransaction = container.resolve(DeleteTransactionService);
+
+    await deleteTransaction.execute(id);
+
+    /** Devolve resposta à requisição */
+    return response
+      .status(200)
+      .json({ message: 'Transaction has been deleted' });
   }
 }
