d00c018413a82329eff00c920f5273612301503a
amaralc <amaral.calil@gmail.com>
2020-12-20 15:52:20 -0300

    FEATURE / Testa rota de import

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
@@ -6,6 +8,9 @@ import { Router } from 'express';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 import TransactionController from '../controllers/TransactionController';
+
+/** Cria instancia do multer para fazer upload */
+const upload = multer(uploadConfig.multer);
 
 const transactionController = new TransactionController();
 
@@ -17,8 +22,13 @@ transactionsRouter.post('/', transactionController.create);
 
 transactionsRouter.delete('/:id', transactionController.delete);
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('csv'),
+  async (request, response) => {
+    // TODO
+    return response.json({ ok: true });
+  },
+);
 
 export default transactionsRouter;
