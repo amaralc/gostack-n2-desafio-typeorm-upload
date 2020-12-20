b91ace519ffde95593e65296fefe8135c33a960d
amaralc <amaral.calil@gmail.com>
2020-12-20 09:43:42 -0300

    FEATURE / Cria categories repository

---
+++ src/repositories/typeorm/CategoriesRepository.ts
@@ -0,0 +1,47 @@
+import { EntityRepository, Repository, getRepository } from 'typeorm';
+
+import Category from '../../models/Category';
+import ICategoriesRepository from '../ICategoriesRepository';
+import ICreateCategoryDTO from '../../dtos/ICreateCategoryDTO'
+
+interface IBalance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+@EntityRepository(Category)
+class TransactionsRepository implements ICategoriesRepository {
+  private ormRepository: Repository<Category>
+
+  constructor() {
+    /** Define ormRepository */
+    this.ormRepository = getRepository(Category);
+  }
+
+ /** Método para criar instancia */
+ public async create(categoryData: ICreateCategoryDTO): Promise<Category> {
+  /** Cria novo instância */
+  const category = this.ormRepository.create(categoryData);
+
+  /** Salva instância no banco de dados */
+  await this.ormRepository.save(category);
+
+  /** Retorna instância criada */
+  return category;
+}
+
+public async findByTitle(title:string): Promise<Category | undefined> {
+  /** Encontra instancia */
+  const category = await this.ormRepository.findOne({
+    where: {
+      title
+    }}
+  );
+
+  /** Return */
+  return category;
+}
+}
+
+export default TransactionsRepository;
