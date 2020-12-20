a1b4140eeb0af4b59c1510bddadf80cff91c33ac
amaralc <amaral.calil@gmail.com>
2020-12-20 11:52:26 -0300

    FEATURE / Atualiza repositorio de categorias

--- src/repositories/typeorm/CategoriesRepository.ts
+++ src/repositories/typeorm/CategoriesRepository.ts
@@ -2,46 +2,40 @@ import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Category from '../../models/Category';
 import ICategoriesRepository from '../ICategoriesRepository';
-import ICreateCategoryDTO from '../../dtos/ICreateCategoryDTO'
-
-interface IBalance {
-  income: number;
-  outcome: number;
-  total: number;
-}
+import ICreateCategoryDTO from '../../dtos/ICreateCategoryDTO';
 
 @EntityRepository(Category)
 class TransactionsRepository implements ICategoriesRepository {
-  private ormRepository: Repository<Category>
+  private ormRepository: Repository<Category>;
 
   constructor() {
     /** Define ormRepository */
     this.ormRepository = getRepository(Category);
   }
 
- /** Método para criar instancia */
- public async create(categoryData: ICreateCategoryDTO): Promise<Category> {
-  /** Cria novo instância */
-  const category = this.ormRepository.create(categoryData);
+  /** Método para criar instancia */
+  public async create(categoryData: ICreateCategoryDTO): Promise<Category> {
+    /** Cria novo instância */
+    const category = this.ormRepository.create(categoryData);
 
-  /** Salva instância no banco de dados */
-  await this.ormRepository.save(category);
+    /** Salva instância no banco de dados */
+    await this.ormRepository.save(category);
 
-  /** Retorna instância criada */
-  return category;
-}
+    /** Retorna instância criada */
+    return category;
+  }
 
-public async findByTitle(title:string): Promise<Category | undefined> {
-  /** Encontra instancia */
-  const category = await this.ormRepository.findOne({
-    where: {
-      title
-    }}
-  );
+  public async findByTitle(title: string): Promise<Category | undefined> {
+    /** Encontra instancia */
+    const category = await this.ormRepository.findOne({
+      where: {
+        title,
+      },
+    });
 
-  /** Return */
-  return category;
-}
+    /** Return */
+    return category;
+  }
 }
 
 export default TransactionsRepository;
