a2f58cf85111fc0f75fc445db5b0d0d35ddc6f73
amaralc <amaral.calil@gmail.com>
2020-12-20 09:49:05 -0300

    FEATURE / Cria servico para criacao de transacoes

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,69 @@
-// import AppError from '../errors/AppError';
+import { injectable, inject, container } from 'tsyringe';
 
+import AppError from '../errors/AppError';
+
+import ICreateTransactionServiceDTO from '../dtos/ICreateTransactionServiceDTO'
+import TransactionsRepository from '../repositories/typeorm/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import FindCategoryByTitle from './FindCategoryIdByTitleService';
+import CreateCategoryService from './CreateCategoryService';
 
+@injectable()
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  constructor(
+    @inject('TransactionsRepository')
+    private transactionsRepository: TransactionsRepository,
+    ) {}
+
+  public async execute({
+    title,
+    type,
+    value,
+    category
+  }: ICreateTransactionServiceDTO): Promise<Transaction> {
+
+    /** Get balance */
+    const balance = await this.transactionsRepository.getBalance();
+
+    /** If balance is not enough, throw error */
+    if (balance.total < value && type === 'outcome') {
+      throw new AppError(
+        'Your current balance is not enough to execute the transaction',
+      );
+    }
+
+    /** Instancia serviço utilizando container de injecao de dependencias */
+    const findCategoryByTitle = container.resolve(
+      FindCategoryByTitle,
+    );
+
+    /** Find category */
+    let selectedCategory = await findCategoryByTitle.execute({title: category})
+
+    /** If doesnt exist, create one */
+    if(!selectedCategory){
+
+      /** Initialize service */
+      const createCategoryService = container.resolve(CreateCategoryService)
+
+      /** Create category */
+      selectedCategory = await createCategoryService.execute({title: category})
+
+    }
+
+    /** Define category id */
+    const category_id = selectedCategory.id
+
+    /** Create transaction */
+    const transaction = await this.transactionsRepository.create({
+      title,
+      type,
+      value,
+      category_id
+    });
+
+    /** Return transaction */
+    return transaction;
   }
 }
 
