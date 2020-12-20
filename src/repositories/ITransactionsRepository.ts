3d2e766f6779438713185f3f65829379c76f8ec5
amaralc <amaral.calil@gmail.com>
2020-12-20 15:05:04 -0300

    FEATURE / Cria interface da classe do repository

--- src/repositories/ITransactionsRepository.ts
+++ src/repositories/ITransactionsRepository.ts
@@ -1,6 +1,8 @@
-import ICreateTransactionDTO from "../dtos/ICreateTransactionDTO";
-import Transaction from "../models/Transaction"
+import ICreateTransactionDTO from '../dtos/ICreateTransactionDTO';
+import Transaction from '../models/Transaction';
 
 export default interface ITransactionsRepository {
   create(transactionData: ICreateTransactionDTO): Promise<Transaction>;
+  findAll(): Promise<Transaction[]>;
+  delete(id: string): Promise<void>;
 }
